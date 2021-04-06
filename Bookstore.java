1e82623bc876cf59309463a050982052b87aa54d
DannyAu <78895522+DannyAu@users.noreply.github.com>
2021-04-07 02:24:58 +0800

    Add files via upload

---
+++ Bookstore.java
@@ -0,0 +1,76 @@
+import java.sql.*;
+import java.util.Scanner;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+import java.io.*;
+
+public class Bookstore{
+	static Database db;
+	private static void update(){
+		try{
+			Scanner input = new Scanner(System.in);
+			System.out.print("Please input the order ID: ");
+			String orderid = input.nextLine();
+			db.orderUpdate(orderid);
+		} catch (Exception e){
+			System.out.println("Error: " + e);
+		}
+	}
+
+	private static void query(){
+		try{
+			Scanner input = new Scanner(System.in);
+			System.out.print("Please input the Month for Order Query (YYYY-MM): ");
+			String orderdate = input.nextLine();
+			db.orderQuery(orderdate);
+		} catch (Exception e){
+			System.out.println("Error: " + e);
+		}
+	}
+
+	private static void popular(){
+		try{
+			Scanner input = new Scanner(System.in);
+			System.out.print("Please input the N popular books number: ");
+			int num = input.nextInt();
+			db.nPopularBook(num);
+		} catch (Exception e){
+			System.out.println("Error: " + e);
+		}
+	}
+
+	public static void request(){
+		Scanner input = new Scanner(System.in);
+		while(true){
+			try{
+				System.out.println("<This is the bookstore interface.>");
+				System.out.println("----------------------------------");
+				System.out.println("1. Order Update.");
+				System.out.println("2. Order Query.");
+				System.out.println("3. N most Popular Book Query.");
+				System.out.println("4. Back to main menu.");
+				System.out.println("");
+				System.out.print("What is your choice??..");
+				int ans = Integer.parseInt(input.nextLine());
+				switch(ans){
+					case 1:
+						update();
+						break;
+					case 2:
+						query();
+						break;
+					case 3:
+						popular();
+						break;
+					case 4:
+						return;
+					default:
+                        System.out.println("error");
+                        break;
+				}
+			} catch (Exception e){
+				System.out.println("[Error]: " + e);
+			}
+		}
+	}
+}
---
+++ Project.java
@@ -0,0 +1,62 @@
+//java -cp .:mysql-connector-java-5.1.47.jar Project
+import java.sql.*;
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.io.*;
+
+
+public class Project {
+    static Database db;
+    static LocalDate date = java.time.LocalDate.now();
+	
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        //LocalDate date = LocalDate.of(2021, 4, 3);
+        while (true) {
+
+            try {  
+                System.out.println("<This is the Book Ordering System.>");
+                System.out.println("-------------------------------------");
+                System.out.println("1. System interface.");
+                System.out.println("2. Customer interface.");
+                System.out.println("3. Bookstore interface.");
+                System.out.println("4. Show System Date.");
+                System.out.println("5. Quit the system......");
+                System.out.println("");
+                System.out.print("Please enter your choice??..");
+                int ans = Integer.parseInt(input.nextLine());
+                switch (ans) {
+                    case 1:
+                        System.out.println(1);
+                        Sys sys = new Sys();
+                        sys.command();
+                        break;
+                    case 2:
+                        System.out.println(2);
+                        Customer customer = new Customer();
+                        customer.command();
+                        break;
+                    case 3:
+                        System.out.println(3);
+                        Bookstore bs = new Bookstore();
+                        bs.request();
+                        break;
+                    case 4:
+                        System.out.print("The System Date is now: ");
+                        System.out.println(date);  
+                        break;
+                    case 5:
+                        return;
+                    default:
+                        System.out.println("error");
+                        break;
+                }
+            }  catch (Exception e) {
+                System.out.println("[Error]: " + e);
+            }
+        }
+    }
+}
+
+
+
